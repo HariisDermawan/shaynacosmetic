ecddbfaa55669f866277a9d25ad28985284247f6
HariisDermawan <harisdermawaan.id@gmail.com>
2026-04-17 01:07:59 +0700

    Enhance BookingTransactionForm with additional customer, delivery, and payment information steps; integrate input validation and file upload

--- app/Filament/Resources/BookingTransactions/Schemas/BookingTransactionForm.php
+++ app/Filament/Resources/BookingTransactions/Schemas/BookingTransactionForm.php
@@ -3,12 +3,15 @@
 namespace App\Filament\Resources\BookingTransactions\Schemas;
 
 use App\Models\Cosmetic;
+use Filament\Forms\Components\FileUpload;
 use Filament\Schemas\Components\Wizard;
 use Filament\Schemas\Components\Wizard\Step;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
 use Filament\Schemas\Components\Grid;
 
 class BookingTransactionForm
@@ -85,6 +88,77 @@ class BookingTransactionForm
                                 ])
 
                         ]),
+
+
+                    Wizard\Step::make('Customer Information')
+                        ->completedIcon('heroicon-m-hand-thumb-up')
+                        ->description('For our marketing')
+                        ->schema([
+                            Grid::make(2)
+                                ->schema([
+                                    TextInput::make('name')
+                                        ->required()
+                                        ->maxLength(255),
+
+                                    TextInput::make('phone')
+                                        ->required()
+                                        ->maxLength(255),
+
+                                    TextInput::make('email')
+                                        ->required()
+                                        ->maxLength(255),
+                                ])
+                        ]),
+
+                    Wizard\Step::make('Delivery Information')
+                        ->completedIcon('heroicon-m-hand-thumb-up')
+                        ->description('Put your correct address')
+                        ->schema([
+                            Grid::make(2)
+                                ->schema([
+                                    TextInput::make('city')
+                                        ->required()
+                                        ->maxLength(255),
+
+                                    TextInput::make('post_code')
+                                        ->required()
+                                        ->maxLength(255),
+
+                                    Textarea::make('address')
+                                        ->required()
+                                        ->maxLength(255),
+                                ]),
+
+
+                        ]),
+
+                    Wizard\Step::make('Payment Information')
+                        ->completedIcon('heroicon-m-hand-thumb-up')
+                        ->description('Review your payment')
+                        ->schema([
+                            Grid::make(3)
+                                ->schema([
+                                    TextInput::make('booking_trx_id')
+                                        ->required()
+                                        ->maxLength(255),
+
+                                    ToggleButtons::make('is_paid')
+                                        ->label('Apakah sudah bayar? ')
+                                        ->boolean()
+                                        ->grouped()
+                                        ->icons([
+                                            true => 'heroicon-o-pencil',
+                                            false => 'heroicon-o-clock'
+                                        ])
+                                        ->required(),
+
+                                    FileUpload::make('proof')
+                                        ->image()
+                                        ->required(),
+                                ]), 
+                        ]),
+
+
 
                 ])
                     ->columnSpanFull()
