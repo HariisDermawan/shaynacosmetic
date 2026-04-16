be519d92fff8e836823876f294a17b4987b2ffd4
HariisDermawan <harisdermawaan.id@gmail.com>
2026-04-17 00:07:43 +0700

    Refactor BookingTransactionForm to use Wizard component for product selection; enhance transaction details schema

--- app/Filament/Resources/BookingTransactions/Schemas/BookingTransactionForm.php
+++ app/Filament/Resources/BookingTransactions/Schemas/BookingTransactionForm.php
@@ -2,10 +2,14 @@
 
 namespace App\Filament\Resources\BookingTransactions\Schemas;
 
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\Toggle;
+use App\Models\Cosmetic;
+use Filament\Schemas\Components\Wizard;
+use Filament\Schemas\Components\Wizard\Step;
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Grid;
 
 class BookingTransactionForm
 {
@@ -13,40 +17,80 @@ class BookingTransactionForm
     {
         return $schema
             ->components([
-                TextInput::make('booking_trx_id')
-                    ->required(),
-                TextInput::make('name')
-                    ->required(),
-                TextInput::make('email')
-                    ->label('Email address')
-                    ->email()
-                    ->required(),
-                TextInput::make('phone')
-                    ->tel()
-                    ->required(),
-                TextInput::make('proof')
-                    ->required(),
-                TextInput::make('post_code')
-                    ->required(),
-                TextInput::make('city')
-                    ->required(),
-                Toggle::make('is_paid')
-                    ->required(),
-                Textarea::make('address')
-                    ->required()
-                    ->columnSpanFull(),
-                TextInput::make('quantity')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('total_amount')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('sub_total_amount')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('total_tax_amount')
-                    ->required()
-                    ->numeric(),
+
+                Wizard::make([
+                    Step::make('product and price')
+                        ->completedIcon('heroicon-m-hand-thumb-up')
+                        ->description('add your product items')
+                        ->schema([
+
+                            Repeater::make('transactionDetails')
+                                ->relationship('transactionDetails')
+                                ->schema([
+                                    Grid::make(2)
+                                        ->schema([
+
+                                            Select::make('cosmetic_id')
+                                                ->relationship('cosmetic', 'name')
+                                                ->searchable()
+                                                ->preload()
+                                                ->required()
+                                                ->label('Select Product')
+                                                ->live()
+                                                ->afterStateUpdated(function ($state, callable $set) {
+                                                    $cosmetic = Cosmetic::find($state);
+                                                    $set('price', $cosmetic ? $cosmetic->price : 0);
+                                                }),
+
+                                            TextInput::make('price')
+                                                ->required()
+                                                ->numeric()
+                                                ->readOnly()
+                                                ->label('Price'),
+
+                                            TextInput::make('quantity')
+                                                ->integer()
+                                                ->default(1)
+                                                ->required(),
+
+                                        ])
+
+                                ])
+                                ->minItems(1)
+                                ->columnSpan('full')
+                                ->label('Choose Products'),
+                            Grid::make(4)
+                                ->schema([
+                                    TextInput::make('quantity')
+                                        ->integer()
+                                        ->label('Total Quantity')
+                                        ->readOnly()
+                                        ->default(1)
+                                        ->required(),
+
+                                    TextInput::make('sub_total_amount')
+                                        ->numeric()
+                                        ->readOnly()
+                                        ->label('Sub Total Amount'),
+
+                                    TextInput::make('total_amount')
+                                        ->numeric()
+                                        ->readOnly()
+                                        ->label('Total Amount'),
+
+                                    TextInput::make('total_tax_amount')
+                                        ->numeric()
+                                        ->readOnly()
+                                        ->label('Total Tax (11%) ')
+                                ])
+
+                        ]),
+
+                ])
+                    ->columnSpanFull()
+                    ->columns(1)
+                    ->skippable()
+
             ]);
     }
 }
