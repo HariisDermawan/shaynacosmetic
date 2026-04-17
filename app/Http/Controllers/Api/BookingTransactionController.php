8fbbb267adcc06952295ae8d7df694e62cd72319
HariisDermawan <harisdermawaan.id@gmail.com>
2026-04-17 16:08:42 +0700

    Implement BookingTransactionController with error handling and add StoreBookingTransactionRequest for validation; enhance Brand, Category, and Cosmetic controllers with resource loading and filtering

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreBookingTransactionRequest;
 
 class BookingTransactionController extends Controller
 {
-    //
+    public function store(StoreBookingTransactionRequest $request){
+        try {
+            //code...
+        } catch (\Throwable $e) {
+            return response()->json([
+                'message' => 'An error occurred',
+                'error'   => $e->getMessage(), 500
+            ]);
+        }
+    }
 }
